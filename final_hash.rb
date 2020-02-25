1839967dcae6ca36b492953377fb1231819e8fa3
Brandon Brown <brandonbrown4792@gmail.com>
2020-02-25 21:31:27 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -1,11 +1,6 @@
 # The final hash will look like this. Earlier methods will expect less as
 # you're suppose to "build up" to this.
 
-def base_hash
-  monopoly = {:railroads => {}}
-  return monopoly
-end
-
 monopoly =  {
   :railroads=>
   {
