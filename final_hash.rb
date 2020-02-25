ec99cb688a35aed843c7d9b84ccdc3ecf4f4cbe1
Brandon Brown <brandonbrown4792@gmail.com>
2020-02-25 21:29:18 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -38,16 +38,3 @@ def base_hash
   monopoly = {:railroads => {}}
   return monopoly
 end
-
-def monopoly_with_second_tier
-  monopoly = {
-    :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {},
-      :names => {}
-    }
-  }
-  return monopoly
-end
-
-def 
